b5453f0f2c1ec2f76928f742577c13ba3bd6bdc3
Gal Gur <gal.gurevich@gmail.com>
2020-05-28 16:52:41 +0300

    centered items, clean unused comments

--- Loby.js
+++ Loby.js
@@ -9,6 +9,7 @@ class Loby extends Phaser.Scene {
         this.load.image('potion3', 'assets/potion3.png');
         this.load.image('potion4', 'assets/potion4.png');
         this.load.image('potions', 'assets/potions.png');
+        this.load.image('potionsY', 'assets/potionsY.png');
         this.load.image('button_spin', 'assets/button_spin.png');
         this.load.image('button_stop', 'assets/button_stop.png');
         this.load.image('slotContainer', 'assets/slotContainer.png');
@@ -17,24 +18,8 @@ class Loby extends Phaser.Scene {
     }
     create() {
 
-        var images = [];
-        /*
-        Group 1 line of potions
-        var group = this.add.group({
-            key: 'potions',
-            frame: [ 0 ],
-            frameQuantity: 1
-        });
-    
-        Phaser.Actions.GridAlign(group.getChildren(), {
-            width: 10,
-            height: 10,
-            cellWidth: 32,
-            cellHeight: 32,
-            x: 100,
-            y: 100
-        });
-        */
+        this.barGroup = this.add.group();
+        this.graphics = this.add.graphics();
 
         //Background Music
         this.soundFX = this.sound.add("bg_music", { loop: "true" });
@@ -59,11 +44,14 @@ class Loby extends Phaser.Scene {
         }, this);
 
         var container = this.add.image(0, 0, 'slotContainer');
-        var slots = this.add.image(0, 0, 'potions');
         var spinBtn = this.add.image(500, 800, 'button_spin');
 
         Phaser.Display.Align.In.Center(container, this.add.zone(500, 500, 1000, 1000));
-        Phaser.Display.Align.In.Center(slots, container);
+
+        for (var i = 0; i < 4; i++) {
+            var bar = this.add.sprite(260 + i * 170, 400, "potionsY");
+            this.barGroup.add(bar);
+        }
 
         spinBtn.setInteractive();
 
@@ -73,21 +61,15 @@ class Loby extends Phaser.Scene {
         {
             gameObject.emit('clicked', gameObject);
         }, this);
-        /*
-        var frames = ['potion1', 'potion2', 'potion3', 'potion4'];
 
-        for (var i = 0; i < frames.length; ++i) 
-        {
-            images[i] = this.add.tileSprite(i * 150, 130, 140, 555, frames[i]);
-            images[i].originX = 0;
-            images[i].originY = 0;
-            Phaser.Display.Align.In.Center(frames, container);
-        }
-        */
     }
 
     clickHandler(spinBtn) {
         spinBtn.setAlpha(0.5);
+    }
+
+    spin() {
+
     }
 
     update(e) {
